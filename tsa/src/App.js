fbed13495fc4ffa278d031b45b258d8d934c1a3a
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-02-11 17:00:10 +0000

    react-router-dom updates
    UPDATES:
    - installed react-router-dom
    - added new pages
    - navigation linked and enabled
    - index and app updated with routing features.

--- tsa/src/App.js
+++ tsa/src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Home from './pages/Home'
+import Tsa from './pages/Tsa'
+import Help from './pages/Help'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/tsa' element={ <Tsa /> } />
+          <Route path='/help' element={ <Help /> } />
+          <Route path='/contact' element={ <Contact /> } />
+          <Route path='/login' element={ <Login /> } />
+          <Route path='/signup' element={ <Signup /> } />
+        </Routes>
+      </div>
     </>
   )
 }
---
+++ tsa/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <>
+      <div>Login</div>
+      <div>
+        <a href='/signup'>click here to signup</a>
+      </div>  
+    </>
+  )
+}
