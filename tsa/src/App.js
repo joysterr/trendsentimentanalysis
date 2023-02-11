5b6541b2a1e016529c67c477bbd267f8895ba330
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-02-11 16:08:47 +0000

    Added Navbar
    UPDATES:
    - added Navbar with css as a component
    - rendered navbar in app interface

--- tsa/src/App.js
+++ tsa/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div>test</div>
-  );
+    <>
+      <Navbar />
+    </>
+  )
 }
 
 export default App;
