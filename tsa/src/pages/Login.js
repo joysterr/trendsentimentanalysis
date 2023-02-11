41e24604639f05e3541bb7ce6afe381926aa97de
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-02-11 20:08:24 +0000

    Updated pages
    UPDATE:
    - added content on pages
    - images added
    - fixed css & html errors in pages

--- tsa/src/pages/Login.js
+++ tsa/src/pages/Login.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Login.css'
 
 export default function Login() {
   return (
     <>
-      <div>Login</div>
-      <div>
-        <Link to='/signup' className='untitled'>
-          click here to sign up
-        </Link>
-      </div>  
+      <div className='login-container'>
+        <div>Login</div>
+        <div>
+          <Link to='/signup' className='untitled'>
+            click here to sign up
+          </Link>
+        </div>  
+      </div>
     </>
   )
 }
--- tsa/src/pages/home.js
+++ tsa/src/pages/home.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <div className='dot'>.</div>
+      <div className='dot'>.</div>
+      <div className='dot'>.</div>
+      <div className='dot'>.</div>
+
+      <h1>Analysis beyond numbers.</h1>
+      <div className='dot'>.</div>
+      <h2>Trend Sentiment Analysis</h2>
+
+      <div className='dot'>.</div>
+      <div className='dot'>.</div>
+      <div className='dot'>.</div>
+      <div className='dot'>.</div>
+
+      <div className='intro-subhead'>
+        Hola! I am tsa. <br/>
+        I was created to understand 'natural language'. <br/>
+        I understand emotions from an array of strings, or "sentences".<br/>
+        Thank you to my creator, joy, who trained me to understand sentiments.<br/>
+        behind these oridinary sequences of 1s & 0s. <br/>
+        Bye ^_^
+      </div>
+      <div className='divider-black'>
+        <h3>What can I do for you?</h3>
+      </div>
+
+      <img src="https://via.placeholder.com/910x500/?text=Hello" alt='placeholder'></img>
+    </div>
   )
 }
