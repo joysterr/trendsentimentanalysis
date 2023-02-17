5564c079562262a7cea28d94886d20fb7a6249e9
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-02-17 14:25:43 +0000

    frontend pages update
    UPDATES:
    - added about page with css file
    - updated contact and help to be merged in one later
    - updated layout on home page
    - fixed img scaling issues. now they are dynamic and can resize automatically based on screen size.

---
+++ tsa/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './About.css'
+
+export default function About() {
+  return (
+    <>
+        <h1>About</h1>
+        <div className='intro-subhead'>
+            हेलो! I am tsa. <br/>
+            I was created to understand 'natural language'. <br/>
+            I understand emotions from an array of strings, or "sentences".<br/>
+            Thank you to my creator, joy, who trained me to understand sentiments<br/>
+            behind these oridinary sequences of 1s & 0s. <br/>
+            Bye ^_^
+        </div>
+    </>
+  )
+}
--- tsa/src/pages/Help.js
+++ tsa/src/pages/Help.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Help() {
   return (
-    <div>Help</div>
+    <>
+      <h1>Help</h1>
+      <div>Need more help?</div>
+      <Link to='/contact' className='untitled'>
+        click here to access form
+      </Link>
+    </>
   )
 }
