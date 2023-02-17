b894d3fcb9096487b9ad22d2c6d0b1ef48c222e6
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-02-17 23:54:00 +0000

    updated twitter_api
    UPDATES:
    -new api keys (temp)
    -updated twitter_api

--- server/twitter_api.py
+++ server/twitter_api.py
@@ -15,36 +15,19 @@ bearer_tok = cfg['twitter_api']['bearer_tok']
 access_tok = cfg['twitter_api']['access_tok']
 access_tok_secret = cfg['twitter_api']['access_tok_secret']
 
+#api v1.1 authorisation
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_tok, access_tok_secret)
 
 api = tweepy.API(auth)
 
-#twitter api v2 auth & call
-api_v2 = tweepy.Client(
-    consumer_key = api_key,
-    consumer_secret = api_key_secret,
-    bearer_token = bearer_tok,
-    access_token = access_tok,
-    access_token_secret = access_tok_secret
-)
+recent_tweets = api.search_tweets(user_search, lang='en', result_type='mixed', count=2, include_entities=False)
 
-#method 1
-recent_tweets = api_v2.search_recent_tweets(
-    "nasa", 
-    max_results = 10, 
-    #extra params
-    media_fields = None,
-    place_fields = None,
-    poll_fields = None,
-    user_auth = True
-)
-
-col = ['Time', 'User', 'Tweet']
+col = ['Time', 'Tweet']
 tweet_data = []
 
-for tweet in recent_tweets.data:
-    tweet_data.append([tweet.tweet_fields.created_at, tweet.user_fields.author_id, tweet.text])
+for tweet in recent_tweets:
+    tweet_data.append([tweet.created_at, tweet.text])
 
 df_tweet_data = pd.DataFrame(tweet_data, columns=col)
 
