e61b3b2c4dbb236199464923c9622322867ab497
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-03-08 01:22:50 +0000

    resolving merge conflicts #8

--- tsa/src/components/Navbar.js
+++ tsa/src/components/Navbar.js
@@ -8,8 +8,7 @@ export default function Navbar() {
         <Link to='/' className='logo'>tsa.</Link>
         <ul>
             <CreateLink to='/tsa'>Tsa</CreateLink>
-            <CreateLink to='/help'>Help</CreateLink>
-            <CreateLink to='/contact'>Contact</CreateLink>
+
             <CreateLink to='/login'>Login</CreateLink>
         </ul>
     </nav>
