64843c83b48a38a4bb55ba3730d2bc10765fd66d
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-03-10 14:44:30 +0000

    Twitter API and backend integration
    update:
    -changes to twitter_api
    -changes to server
    - integration successful

--- server/twitter_api.py
+++ server/twitter_api.py
@@ -1,38 +1,44 @@
-import pandas as pd
-import configparser
-import tweepy
+def exe_tapi(input_search):
+    import pandas as pd
+    import configparser
+    import tweepy
 
-#import from server
-user_search = '' #user query goes here
+    #import from server
+    user_search = input_search #user query goes here
 
-#auth twitter api v1/1.1
-cfg = configparser.ConfigParser()
-cfg.read('config.ini')
+    # if user_search == '':
+    #     quit()
+    # else:
+    #     print('success')
 
-api_key = cfg['twitter_api']['api_key']
-api_key_secret = cfg['twitter_api']['api_key_secret']
-bearer_tok = cfg['twitter_api']['bearer_tok']
-access_tok = cfg['twitter_api']['access_tok']
-access_tok_secret = cfg['twitter_api']['access_tok_secret']
+    #auth twitter api v1/1.1
+    cfg = configparser.ConfigParser()
+    cfg.read('config.ini')
 
-#api v1.1 authorisation
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_tok, access_tok_secret)
+    api_key = cfg['twitter_api']['api_key']
+    api_key_secret = cfg['twitter_api']['api_key_secret']
+    bearer_tok = cfg['twitter_api']['bearer_tok']
+    access_tok = cfg['twitter_api']['access_tok']
+    access_tok_secret = cfg['twitter_api']['access_tok_secret']
 
-api = tweepy.API(auth)
+    #api v1.1 authorisation
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth.set_access_token(access_tok, access_tok_secret)
 
-recent_tweets = api.search_tweets(user_search, lang='en', result_type='mixed', count=5, include_entities=False)
+    api = tweepy.API(auth)
 
-col = ['Time', 'Tweet']
-tweet_data = []
+    recent_tweets = api.search_tweets(user_search, lang='en', result_type='mixed', count=5, include_entities=False)
 
-for tweet in recent_tweets:
-    tweet_data.append([tweet.created_at, tweet.text])
+    col = ['Time', 'Tweet']
+    tweet_data = []
 
-df_tweet_data = pd.DataFrame(tweet_data, columns=col)
+    for tweet in recent_tweets:
+        tweet_data.append([tweet.created_at, tweet.text])
 
-print(df_tweet_data)
+    df_tweet_data = pd.DataFrame(tweet_data, columns=col)
 
-#export data/results
-df_tweet_data.to_csv('results.csv')
-df_tweet_data.to_json('results.json')
+    print(df_tweet_data)
+
+    #export data/results
+    df_tweet_data.to_csv('tweets_export/results.csv')
+    df_tweet_data.to_json('tweets_export/results.json')
