b6773bd94a74411b3c9de72111359cbc35074e5b
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-03-10 13:29:59 +0000

    updates to frontend
    updates:
    - added poetry component
    - updated help, home and contact pages.
    - updated experiments pages
    - more

--- tsa/src/pages/Contact.js
+++ tsa/src/pages/Contact.js
@@ -1,9 +1,41 @@
 import React from 'react'
+//import axios from "axios";
+import './Contact.css'
 
 export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('okay, we got your cry for help')
+  }
+
   return (
     <>
-      <h1>Contact Us</h1>
+      <div className='contact-container'>
+        <div className='contact-box'>
+          <div className='login-form'>
+            <form className='loginForm' id='loginForm'>
+              <input
+                type='email'
+                id='inputEmail'
+                placeholder='email address'
+                name='inputEmail'
+              />
+              <br/>
+              <textarea
+                type=''
+                id='inputQuery'
+                placeholder='Type your query'
+                name='inputquery'
+                className='query-input'
+              />
+              <br/>
+              <button id='btn-login' type='submit' onClick={handleSubmit}>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>  
+      </div>
     </>
   )
 }
--- tsa/src/pages/Help.js
+++ tsa/src/pages/Help.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Contact from './Contact'
+import './Help.css'
 
 export default function Help() {
   return (
     <>
-      <h1>Help</h1>
-      <div>Need more help?</div>
-      <Link to='/contact' className='untitled'>
-        click here to access form
-      </Link>
+      <h1>Who am I? What am I? Why am I?</h1> <br/>
+      <p>TrendSentimentAnalysis is Magna duis duis quis fugiat tempor dolore et enim nostrud ut aliquip irure. Nisi mollit commodo fugiat elit culpa deserunt aute consectetur laboris labore consectetur ea. Fugiat incididunt id aute excepteur velit tempor labore tempor proident velit ullamco consectetur. Officia laborum consequat duis duis anim nostrud sunt est duis amet aliqua commodo sunt. Qui nisi ipsum adipisicing sunt consequat ad velit consectetur tempor aliqua consectetur consectetur amet officia. Duis sint elit eu nisi exercitation laboris ut qui duis aliqua mollit laborum irure.</p>
+      <p>TrendSentimentAnalysis is Magna duis duis quis fugiat tempor dolore et enim nostrud ut aliquip irure. Nisi mollit commodo fugiat elit culpa deserunt aute consectetur laboris labore consectetur ea. Fugiat incididunt id aute excepteur velit tempor labore tempor proident velit ullamco consectetur. Officia laborum consequat duis duis anim nostrud sunt est duis amet aliqua commodo sunt. Qui nisi ipsum adipisicing sunt consequat ad velit consectetur tempor aliqua consectetur consectetur amet officia. Duis sint elit eu nisi exercitation laboris ut qui duis aliqua mollit laborum irure.</p>
+      <p>TrendSentimentAnalysis is Magna duis duis quis fugiat tempor dolore et enim nostrud ut aliquip irure. Nisi mollit commodo fugiat elit culpa deserunt aute consectetur laboris labore consectetur ea. Fugiat incididunt id aute excepteur velit tempor labore tempor proident velit ullamco consectetur. Officia laborum consequat duis duis anim nostrud sunt est duis amet aliqua commodo sunt. Qui nisi ipsum adipisicing sunt consequat ad velit consectetur tempor aliqua consectetur consectetur amet officia. Duis sint elit eu nisi exercitation laboris ut qui duis aliqua mollit laborum irure.</p>
+      <p>TrendSentimentAnalysis is Magna duis duis quis fugiat tempor dolore et enim nostrud ut aliquip irure. Nisi mollit commodo fugiat elit culpa deserunt aute consectetur laboris labore consectetur ea. Fugiat incididunt id aute excepteur velit tempor labore tempor proident velit ullamco consectetur. Officia laborum consequat duis duis anim nostrud sunt est duis amet aliqua commodo sunt. Qui nisi ipsum adipisicing sunt consequat ad velit consectetur tempor aliqua consectetur consectetur amet officia. Duis sint elit eu nisi exercitation laboris ut qui duis aliqua mollit laborum irure.</p>
+      <p>TrendSentimentAnalysis is Magna duis duis quis fugiat tempor dolore et enim nostrud ut aliquip irure. Nisi mollit commodo fugiat elit culpa deserunt aute consectetur laboris labore consectetur ea. Fugiat incididunt id aute excepteur velit tempor labore tempor proident velit ullamco consectetur. Officia laborum consequat duis duis anim nostrud sunt est duis amet aliqua commodo sunt. Qui nisi ipsum adipisicing sunt consequat ad velit consectetur tempor aliqua consectetur consectetur amet officia. Duis sint elit eu nisi exercitation laboris ut qui duis aliqua mollit laborum irure.</p>
+      <p>TrendSentimentAnalysis is Magna duis duis quis fugiat tempor dolore et enim nostrud ut aliquip irure. Nisi mollit commodo fugiat elit culpa deserunt aute consectetur laboris labore consectetur ea. Fugiat incididunt id aute excepteur velit tempor labore tempor proident velit ullamco consectetur. Officia laborum consequat duis duis anim nostrud sunt est duis amet aliqua commodo sunt. Qui nisi ipsum adipisicing sunt consequat ad velit consectetur tempor aliqua consectetur consectetur amet officia. Duis sint elit eu nisi exercitation laboris ut qui duis aliqua mollit laborum irure.</p>
+      <p>TrendSentimentAnalysis is Magna duis duis quis fugiat tempor dolore et enim nostrud ut aliquip irure. Nisi mollit commodo fugiat elit culpa deserunt aute consectetur laboris labore consectetur ea. Fugiat incididunt id aute excepteur velit tempor labore tempor proident velit ullamco consectetur. Officia laborum consequat duis duis anim nostrud sunt est duis amet aliqua commodo sunt. Qui nisi ipsum adipisicing sunt consequat ad velit consectetur tempor aliqua consectetur consectetur amet officia. Duis sint elit eu nisi exercitation laboris ut qui duis aliqua mollit laborum irure.</p>
+      
+      <div className='divider-black'>
+        <h3>Contact + Support</h3>
+      </div>
+
+      <div className='contact-container'>
+        <Contact />
+      </div>
+      <div className='message-end'>
+        <p>(we aim to get back to you within 24hours)</p>
+      </div>
     </>
   )
 }
