02c68b6ab2daf1ed47ccaf91c0e321e464b198fd
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-03-11 11:16:45 +0000

    integration task#1

--- server/server.py
+++ server/server.py
@@ -1,9 +1,11 @@
-#imports
+# imports
 from flask import Flask
 from flask import request
+import twitter_api as tapi
 
 app = Flask(__name__)
 
+# http requests handling
 @app.route('/')
 def test():
     return('welcome to tsa!')
@@ -13,7 +15,10 @@ def search():
     if request.method == 'POST':
         data = request.get_data(as_text=True)
         print(data, type(data))
+        tapi.exec_tapi(data)
     return('200: OK')
 
+
+# exec server
 if (__name__) ==  '__main__':
     app.run(debug=True)
